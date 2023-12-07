93e79a92503b2f2d32bd06604152f94a673b2f87
purple1027 <purple1027@naver.com>
2023-12-07 17:49:08 +0900

    6장 객체

---
+++ javaBasic03/src/javaBasic03/자동차판매현황.java
@@ -0,0 +1,35 @@
+package javaBasic03;
+
+public class 자동차판매현황 {
+
+	public 자동차판매현황(String string, int i, int j, int k, int l, int m) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// 1. 필드 만들기
+		
+	String 차랑명;
+	int 차량총액;
+	int 인도금;
+	int 할부원금;
+	int 상환기간;
+	int 월납입금;
+	
+	
+	// setter, getter 만들기 (소스에서 ~)
+	
+	
+	
+		
+		
+		
+		
+		
+		
+		
+	} //메인
+
+} // 클래스
