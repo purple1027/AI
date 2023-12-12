1ae474c882787ccecbfd736b68bbc48f3e99ccde
purple1027 <purple1027@naver.com>
2023-12-12 09:36:36 +0900

    파이썬 변수 문자열

---
+++ hello.py
@@ -0,0 +1,14 @@
+print("Hello Python")
+print(100+20)
+
+name="김콩쥐"
+address="부산시 해운대구"
+phonenumber="010-1234-5678"
+email="book@inforbook.com"
+print(name)
+print(address)
+print(phonenumber)
+print(email)
+
+
+
